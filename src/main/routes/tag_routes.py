f1b2222e14db5be67b9e3afe6482c6eb6dc43251
Alexandre Junior <alejunior.dev@gmail.com>
2024-02-09 23:02:11 -0300

    feat: implements error handler

--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -3,14 +3,21 @@ from flask import Blueprint, jsonify, request
 from src.views.http_types.http_request import HttpRequest
 from src.views.tag_creator_view import TagCreatorView
 
+from src.errors.error_handler import handle_errors
+
 tags_routes_bp = Blueprint("tags_routes", __name__)
 
 
 @tags_routes_bp.route("/create_tag", methods=["POST"])
 def create_tags():
-    tag_creator_view = TagCreatorView()
-    http_request = HttpRequest(body=request.json)
+    response = None
 
-    response = tag_creator_view.validate_and_create(http_request)
+    try:
+        tag_creator_view = TagCreatorView()
+        http_request = HttpRequest(body=request.json)
+
+        response = tag_creator_view.validate_and_create(http_request)
+    except Exception as execption:
+        response = handle_errors(execption)
 
     return jsonify(response.body), response.status_code
